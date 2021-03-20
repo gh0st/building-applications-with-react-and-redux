6c4ea5850ffda8b3346fb99b84171426b3c9cf79
Chad Hollman <hollmanchad@gmail.com>
2021-03-20 12:58:09 -0400

    tell webpack to lint code on save

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -33,7 +33,7 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ['babel-loader'],
+        use: ['babel-loader', 'eslint-loader'],
       },
       {
         test: /(\.css)$/,
