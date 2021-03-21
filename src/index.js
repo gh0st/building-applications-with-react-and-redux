6584931b2da77c18a8a981f7b10da1cf8b09aa84
Chad Hollman <hollmanchad@gmail.com>
2021-03-21 16:28:52 -0400

    now our app will be able to access our redux store

--- src/index.js
+++ src/index.js
@@ -4,10 +4,18 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './components/App';
 import './index.css';
+import configureStore from './redux/configureStore';
+import { Provider as ReduxProvider } from 'react-redux'; // this will provide Redux store data to our React components
+
+// it can be useful to pass initial state into the store here if you're server
+// rendering or initializing your Redux store with data from localStorage
+const store = configureStore();
 
 render(
-  <Router>
-    <App />
-  </Router>,
+  <ReduxProvider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </ReduxProvider>,
   document.getElementById('app')
 );
