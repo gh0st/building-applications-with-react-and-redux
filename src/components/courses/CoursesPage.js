02a4e6b444205cbd45219f6a991dfac77ffa5924
Chad Hollman <hollmanchad@gmail.com>
2021-03-21 18:11:24 -0400

    refactoring couses page to be a container, fetching courses from api with new thunk

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -5,39 +5,20 @@ import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 
 class CoursesPage extends React.Component {
-  state = {
-    course: {
-      title: ""
-    }
-  };
-
-  handleChange = event => {
-    const course = { ...this.state.course, title: event.target.value };
-    this.setState({ course });
-  }
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    // since we didn't declare mapDispatchToProps, connect automatically adds Dispatch as a prop
-    // remember: you have to dispatch an action. if you just call an action creator it won't do
-    // anything. action creators just return an object
-    // we don't need to call dispatch here since that's being handled in
-    // mapDispatchToProps now
-    this.props.actions.createCourse(this.state.course);
+  componentDidMount() {
+    this.props.actions.loadCourses().catch(error => {
+      alert("Loading courses failed " + error);
+    });
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <h2>Courses</h2>
-        <h3>Add Course</h3>
-        <input type="text" onChange={this.handleChange} value={this.state.course.title} />
-        <input type="submit" value="Save" />
-
-        {this.props.courses.map(course => (
-          <div key={course.title}>{course.title}</div>
-        ))}
-      </form>
+      <>
+      <h2>Courses</h2>
+      {this.props.courses.map(course => (
+        <div key={course.title}>{course.title}</div>
+      ))}
+      </>
     );
   }
 }
