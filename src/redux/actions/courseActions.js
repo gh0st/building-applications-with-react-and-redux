beda7ab2732d38ffef155503a46c8c0b78b7cc65
Chad Hollman <hollmanchad@gmail.com>
2021-03-21 18:09:03 -0400

    create first thunk

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,5 +1,21 @@
 import * as types from "./actionTypes";
+import * as courseApi from "../../api/courseApi";
 
 export function createCourse(course) {
   return  { type: types.CREATE_COURSE, course };
+}
+
+export function loadCourseSuccess(courses) {
+  return { type: types.LOAD_COURSES_SUCCESS, courses };
+}
+
+export function loadCourses() {
+  // redux thunk injects dispatch so we don't have to
+  return function(dispatch) {
+    return courseApi.getCourses().then(courses => {
+      dispatch(loadCourseSuccess(courses));
+    }).catch(error => {
+      throw error;
+    });
+  }
 }
