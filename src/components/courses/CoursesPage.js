58101076ef8d1828328431e475a81bf17a0cc759
Chad Hollman <hollmanchad@gmail.com>
2021-03-21 16:54:45 -0400

    implement map dispatch to props

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -20,7 +20,9 @@ class CoursesPage extends React.Component {
     // since we didn't declare mapDispatchToProps, connect automatically adds Dispatch as a prop
     // remember: you have to dispatch an action. if you just call an action creator it won't do
     // anything. action creators just return an object
-    this.props.dispatch(courseActions.createCourse(this.state.course));
+    // we don't need to call dispatch here since that's being handled in
+    // mapDispatchToProps now
+    this.props.createCourse(this.state.course);
   }
 
   render() {
@@ -41,7 +43,9 @@ class CoursesPage extends React.Component {
 
 CoursesPage.propTypes = {
   courses: PropTypes.array.isRequired,
-  dispatch: PropTypes.func.isRequired
+  // since we declared mapDispatchToProps, dispatch is no longer injected.
+  // only the actions we declared in mapDispatchToProps are passed in.
+  createCourse: PropTypes.func.isRequired
 };
 
 //this func determines what state is passed to our component via props
@@ -51,5 +55,15 @@ function mapStateToProps(state) {
   }
 }
 
+function mapDispatchToProps(dispatch) {
+  return {
+    // remember, if you don't call dispatch, nothing will happen. action
+    // creators must be called by dispatch
+    // dispatch is the function that notifies redux about an action
+    createCourse: course => dispatch(courseActions.createCourse(course))
+  }
+}
+
 // when we omit mapDispatchToProps, our componenet gets a dispatch prop injected automatically
-export default connect(mapStateToProps)(CoursesPage);
+// mapDispatchToProps determines what actions are available on props in our component
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
