0c3c657ba6028df4df6b7b32e1347bbf38046a89
Chad Hollman <hollmanchad@gmail.com>
2021-03-21 16:05:32 -0400

    add a handle submit method

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -12,9 +12,14 @@ class CoursesPage extends React.Component {
     this.setState({ course });
   }
 
+  handleSubmit = (event) => {
+    event.preventDefault();
+    alert(this.state.course.title);
+  }
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <h2>Courses</h2>
         <h3>Add Course</h3>
         <input type="text" onChange={this.handleChange} value={this.state.course.title} />
