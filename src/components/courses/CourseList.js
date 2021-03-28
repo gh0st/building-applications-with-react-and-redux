6dd20f61d1abaf5be28eda2a8e257ebe9830a026
Chad Hollman <hollmanchad@gmail.com>
2021-03-28 18:23:45 -0400

    implement a delete button with handlers

--- src/components/courses/CourseList.js
+++ src/components/courses/CourseList.js
@@ -1,8 +1,8 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-const CourseList = ({ courses }) => (
+const CourseList = ({ courses, onDeleteClick }) => (
   <table className="table">
     <thead>
       <tr>
@@ -10,25 +10,34 @@ const CourseList = ({ courses }) => (
         <th>Title</th>
         <th>Author</th>
         <th>Category</th>
+        <th />
       </tr>
     </thead>
     <tbody>
-      {courses.map(course => {
+      {courses.map((course) => {
         return (
           <tr key={course.id}>
             <td>
               <a
                 className="btn btn-light"
-                href={"http://pluralsight.com/courses/" + course.slug}
+                href={'http://pluralsight.com/courses/' + course.slug}
               >
                 Watch
               </a>
             </td>
             <td>
-              <Link to={"/course/" + course.slug}>{course.title}</Link>
+              <Link to={'/course/' + course.slug}>{course.title}</Link>
             </td>
             <td>{course.authorName}</td>
             <td>{course.category}</td>
+            <td>
+              <button
+                className="btn btn-outline-danger"
+                onClick={() => onDeleteClick(course)}
+              >
+                Delete
+              </button>
+            </td>
           </tr>
         );
       })}
@@ -37,7 +46,8 @@ const CourseList = ({ courses }) => (
 );
 
 CourseList.propTypes = {
-  courses: PropTypes.array.isRequired
+  courses: PropTypes.array.isRequired,
+  onDeleteClick: PropTypes.func.isRequired,
 };
 
 export default CourseList;
--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -7,44 +7,55 @@ import { bindActionCreators } from 'redux';
 import CourseList from './CourseList';
 import { Redirect } from 'react-router-dom';
 import Spinner from '../common/Spinner';
+import { toast } from 'react-toastify';
 
 class CoursesPage extends React.Component {
   state = {
-    redirectToAddCoursePage: false
+    redirectToAddCoursePage: false,
   };
 
   componentDidMount() {
     const { courses, authors, actions } = this.props;
     if (courses.length === 0) {
-      actions.loadCourses().catch(error => {
-        alert("Loading courses failed " + error);
+      actions.loadCourses().catch((error) => {
+        alert('Loading courses failed ' + error);
       });
     }
 
     if (authors.length === 0) {
-      actions.loadAuthors().catch(error => {
-        alert("Loading authors failed " + error);
+      actions.loadAuthors().catch((error) => {
+        alert('Loading authors failed ' + error);
       });
     }
   }
 
+  handleDeleteCourse = (course) => {
+    toast.success('Course deleted');
+    this.props.actions.deleteCourse(course);
+  };
+
   render() {
     return (
       <>
-      {this.state.redirectToAddCoursePage && <Redirect to="/course" />}
-      <h2>Courses</h2>
-      {this.props.loading
-        ? (<Spinner />)
-        : (
-          <><button
-            style={{ marginBottom: 20 }}
-            className="btn btn-primary add-course"
-            onClick={() => this.setState({ redirectToAddCoursePage: true })}
-          >Add Course</button>
-          <CourseList courses={this.props.courses} />
+        {this.state.redirectToAddCoursePage && <Redirect to="/course" />}
+        <h2>Courses</h2>
+        {this.props.loading ? (
+          <Spinner />
+        ) : (
+          <>
+            <button
+              style={{ marginBottom: 20 }}
+              className="btn btn-primary add-course"
+              onClick={() => this.setState({ redirectToAddCoursePage: true })}
+            >
+              Add Course
+            </button>
+            <CourseList
+              courses={this.props.courses}
+              onDeleteClick={this.handleDeleteCourse}
+            />
           </>
-        )
-      }
+        )}
       </>
     );
   }
@@ -56,23 +67,25 @@ CoursesPage.propTypes = {
   // since we declared mapDispatchToProps, dispatch is no longer injected.
   // only the actions we declared in mapDispatchToProps are passed in.
   actions: PropTypes.object.isRequired,
-  loading: PropTypes.bool.isRequired
+  loading: PropTypes.bool.isRequired,
 };
 
 //this func determines what state is passed to our component via props
 function mapStateToProps(state) {
   return {
-    courses: state.authors.length === 0
-      ?  []
-      : state.courses.map(course => {
-        return {
-          ...course,
-          authorName: state.authors.find(a => a.id === course.authorId).name
-        }
-      }), // be specific, request only the data your component needs
+    courses:
+      state.authors.length === 0
+        ? []
+        : state.courses.map((course) => {
+            return {
+              ...course,
+              authorName: state.authors.find((a) => a.id === course.authorId)
+                .name,
+            };
+          }), // be specific, request only the data your component needs
     authors: state.authors,
-    loading: state.apiCallsInProgress > 0
-  }
+    loading: state.apiCallsInProgress > 0,
+  };
 }
 
 function mapDispatchToProps(dispatch) {
@@ -82,9 +95,10 @@ function mapDispatchToProps(dispatch) {
     // dispatch is the function that notifies redux about an action
     actions: {
       loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
-      loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch)
-    }
-  }
+      loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch),
+      deleteCourse: bindActionCreators(courseActions.deleteCourse, dispatch),
+    },
+  };
 }
 
 // when we omit mapDispatchToProps, our componenet gets a dispatch prop injected automatically
