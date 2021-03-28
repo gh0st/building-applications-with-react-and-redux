71b38a7e36f85a02de2475a16735cdd9364f1b35
Chad Hollman <hollmanchad@gmail.com>
2021-03-28 18:00:28 -0400

    now if an error occurs, our api status will get updated

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,6 +1,6 @@
 import * as types from './actionTypes';
 import * as courseApi from '../../api/courseApi';
-import { beginApiCall } from './apiStatusActions';
+import { beginApiCall, apiCallError } from './apiStatusActions';
 
 export function loadCourseSuccess(courses) {
   return { type: types.LOAD_COURSES_SUCCESS, courses };
@@ -41,6 +41,7 @@ export function saveCourse(course) {
           : dispatch(createCourseSuccess(savedCourse));
       })
       .catch((error) => {
+        dispatch(apiCallError(error));
         throw error;
       });
   };
