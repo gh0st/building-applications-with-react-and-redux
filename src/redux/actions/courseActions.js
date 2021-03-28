273e2d448fee05c74b935345c4a1ae843352709a
Chad Hollman <hollmanchad@gmail.com>
2021-03-28 18:18:23 -0400

    implement course delete reducer

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -14,6 +14,10 @@ export function updateCourseSuccess(course) {
   return { type: types.UPDATE_COURSE_SUCCESS, course };
 }
 
+export function deleteCourseOptimistic(course) {
+  return { type: types.DELETE_COURSE_OPTIMISTIC, course };
+}
+
 export function loadCourses() {
   // redux thunk injects dispatch so we don't have to
   return function (dispatch) {
@@ -45,5 +49,12 @@ export function saveCourse(course) {
         dispatch(apiCallError(error));
         throw error;
       });
+  };
+}
+
+export function deleteCourse(course) {
+  return function (dispatch) {
+    dispatch(deleteCourseOptimistic(course));
+    return courseApi.deleteCourse(course.id);
   };
 }
