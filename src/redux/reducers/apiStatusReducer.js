9631a67aa1e347b81a2efd68734c402f226b4980
Chad Hollman <hollmanchad@gmail.com>
2021-03-28 17:59:26 -0400

    decrement api calls if an error is thrown

--- src/redux/reducers/apiStatusReducer.js
+++ src/redux/reducers/apiStatusReducer.js
@@ -1,8 +1,8 @@
-import * as types from "../actions/actionTypes";
-import initialState from "./initialState";
+import * as types from '../actions/actionTypes';
+import initialState from './initialState';
 
 function actionTypeEndsInSuccess(type) {
-  return type.substring(type.length - 8) === "_SUCCESS";
+  return type.substring(type.length - 8) === '_SUCCESS';
 }
 
 export default function apiCallStatusReducer(
@@ -11,7 +11,10 @@ export default function apiCallStatusReducer(
 ) {
   if (action.type == types.BEGIN_API_CALL) {
     return state + 1;
-  } else if (actionTypeEndsInSuccess(action.type)) {
+  } else if (
+    action.type === types.API_CALL_ERROR ||
+    actionTypeEndsInSuccess(action.type)
+  ) {
     return state - 1;
   }
 
